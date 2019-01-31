c4f3d87f012e7bd93279b74adf7aa7b1e9d09034
Radoslav Benda <radoslav.benda@zonedigital.com>
2019-01-31 22:20:17 +0000

    remove unnecessary font import + formatting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 /*eslint-disable no-console */
 /*eslint-disable no-unused-vars */
 import './index.css';
-import './css/bootstrap.css'
-import './css/thumbnail-gallery.css'
-import './fonts/glyphicons-halflings-regular.eot'
+import './css/bootstrap.css';
+import './css/thumbnail-gallery.css';
 
 import bootstrap from 'bootstrap';
 import angular from 'angular';
@@ -13,30 +12,29 @@ import ngResource from 'angular-resource';
 import mainController from './app/mainController';
 import giphyService from './app/giphyService';
 
-let app = angular.module("giphyApp", ["ngRoute", "ngResource"]);
+let app = angular.module('giphyApp', ['ngRoute', 'ngResource']);
 
 let exceptionHandler = function($injector) {
-    return function(exception, cause) {
-        console.error(exception, cause, exception.stack);
-    };
+  return function(exception, cause) {
+    console.error(exception, cause, exception.stack);
+  };
 };
 
-app.factory("giphyService", ["$http", giphyService]);
-app.controller("mainController", ["giphyService", mainController]);
+app.factory('giphyService', ['$http', giphyService]);
+app.controller('mainController', ['giphyService', mainController]);
 
-app.config(["$routeProvider", function($routeProvider) {
+app.config([
+  '$routeProvider', function($routeProvider) {
 
-    $routeProvider
-        .when("/favourites", {
-            templateUrl: 'templates/favourites.html',
-            controller: "mainController",
-            controllerAs: "main"
-        })
-        .when("/", {
-            templateUrl: 'templates/mainPanel.html',
-            controller: "mainController",
-            controllerAs: "main"
-        });
-}]);
+    $routeProvider.when('/favourites', {
+      templateUrl: 'templates/favourites.html',
+      controller: 'mainController',
+      controllerAs: 'main',
+    }).when('/', {
+      templateUrl: 'templates/mainPanel.html',
+      controller: 'mainController',
+      controllerAs: 'main',
+    });
+  }]);
 
-app.factory("$exceptionHandler", ["$injector", exceptionHandler]);
+app.factory('$exceptionHandler', ['$injector', exceptionHandler]);
